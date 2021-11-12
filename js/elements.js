2891a65b612237483b261ae3690ed19bfc639349
michaeldidonato <83918325+michaeldidonato@users.noreply.github.com>
2021-11-12 17:31:13 +0100

    Add files via upload

---
+++ js/elements.js
@@ -0,0 +1,36 @@
+let mainDiv = document.querySelector('#divRow')
+console.log(mainDiv);
+
+function createCards(title,url,timeUnix) {
+let mainDivCard = document.createElement('div');
+mainDivCard.setAttribute('class','col-md-4');
+mainDiv.appendChild(mainDivCard);
+
+let divCard = document.createElement('div');
+divCard.setAttribute('class','card-body border-card m-3')
+mainDivCard.appendChild(divCard);
+
+let titleLink = document.createElement('h5');
+titleLink.setAttribute('class','card-title color-title')
+titleLink.innerHTML = title;
+divCard.appendChild(titleLink);
+
+let unixTimestamp = timeUnix;
+let milliseconds = unixTimestamp * 1000;
+let dateObject = new Date(milliseconds);
+let humanDateFormat = dateObject.toLocaleString();
+console.log(humanDateFormat);
+let showTime = document.createElement('p');
+showTime.setAttribute('class','card-text');
+showTime.innerHTML = humanDateFormat;
+divCard.appendChild(showTime);
+
+let showUrl = document.createElement('a');
+showUrl.setAttribute('href', url);
+divCard.appendChild(showUrl);
+let buttonUrl = document.createElement('button');
+buttonUrl.setAttribute('class','btn btn-secondary')      
+buttonUrl.innerHTML = 'Read more';                 
+showUrl.appendChild(buttonUrl);
+
+}
---
+++ js/index.js
@@ -0,0 +1,30 @@
+let start = 0;
+let i = start;
+let end = start+10;
+
+async function getValue() {
+  let response = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json');
+  let dataId = await response.json();
+  
+  for(; i<end; i++){
+    console.log(dataId[i]);
+    let article = await fetch(`https://hacker-news.firebaseio.com/v0/item/${dataId[i]}.json`);
+    let resObj = await article.json();
+    console.log(resObj.title);
+    console.log(resObj.url);
+    console.log(resObj.time);
+    createCards(resObj.title, resObj.url, resObj.time);
+  }
+}
+
+getValue();
+
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', ()=>{
+  start = start + 10;
+  end = end + 10;
+  getValue();
+})
+
+
+
