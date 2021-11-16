aa01c1d035434a0cf9e381ed272dab2ec45ddc68
michaeldidonato <83918325+michaeldidonato@users.noreply.github.com>
2021-11-16 22:01:30 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,21 +4,26 @@ let end = start+10;
 
 async function getValue() {
   let response = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json');
-  let dataId = await response.json();
+  if (response.ok) {
+    let dataId = await response.json();
 
-  for(; i<end; i++){
-    console.log(dataId[i]);
-    let article = await fetch(`https://hacker-news.firebaseio.com/v0/item/${dataId[i]}.json`);
-      if(article.ok){
-        let resObj = await article.json();
-        console.log(resObj.title);
-        console.log(resObj.url);
-        console.log(resObj.time);
-        createCards(resObj.title, resObj.url, resObj.time);
+    for(; i<end; i++){
+      console.log(dataId[i]);
+      let article = await fetch(`https://hacker-news.firebaseio.com/v0/item/${dataId[i]}.json`);
+        if(article.ok){
+          let resObj = await article.json();
+          console.log(resObj.title);
+          console.log(resObj.url);
+          console.log(resObj.time);
+          createCards(resObj.title, resObj.url, resObj.time);
+        }
+      else {
+        alert("HTTP-Error: " + article.status)
       }
-    else {
-      alert("HTTP-Error: " + article.status)
     }
+  }
+  else {
+    alert("HTTP-Error: " + response.status)
   }
 }
 
