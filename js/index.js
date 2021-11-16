a85a42ea9928e0713dbe5ba713e71d7cd07399b3
michaeldidonato <83918325+michaeldidonato@users.noreply.github.com>
2021-11-16 14:21:13 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let end = start+10;
 async function getValue() {
   let response = await fetch('https://hacker-news.firebaseio.com/v0/newstories.json');
   let dataId = await response.json();
-  
+
   for(; i<end; i++){
     console.log(dataId[i]);
     let article = await fetch(`https://hacker-news.firebaseio.com/v0/item/${dataId[i]}.json`);
@@ -25,6 +25,3 @@ btn.addEventListener('click', ()=>{
   end = end + 10;
   getValue();
 })
-
-
-
